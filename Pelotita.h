64ade514d26cd0be68a4d54bf5da37a6f97b65b3
mac21 <mac21@mac21-Dell.(none)>
2014-03-27 13:52:33 -0600

    primer commit

---
+++ Pelotita.h
@@ -0,0 +1,26 @@
+#ifndef PELOTITA_H
+#define PELOTITA_H
+#include "SDL/SDL.h"
+
+const int SCREEN_WIDTH = 540;
+const int SCREEN_HEIGHT = 480;
+
+class Pelotita
+{
+    public:
+        int x,y;
+        SDL_Surface*surface;
+        int velocidad_x,velocidad_y;
+        Pelotita*sig;
+        bool en_pantalla;
+        int width;
+        int height;
+        Pelotita(int x,int y,int velocidad_x,
+          int velocidad_y,SDL_Surface*surface);
+        virtual ~Pelotita();
+        void moverse();
+    protected:
+    private:
+};
+
+#endif // PELOTITA_H
